5a671b1c4a8e0480c266549f645d0b315af30409
Nikrator <nikitatrussov@gmail.com>
2026-09-21 20:35:08 +0600

    Дождался студеческой почты, поэтому коммичу все уже созданное туда

---
+++ lab_2.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <windows.h>
+int current_day = 1;
+int current_hour = 8;
+int activeCall;
+int gameState = 0;
+int MAX_SLOTS = 10;
+
+//Инвентарь и его размер
+Item inventory[MAX_SLOTS] = {0};
+
+
+//Создаем составной тип данных под предметы
+typedef struct {
+    char name[30];
+    int quantity;
+    char description;
+} Item;
+
+int main() {
+
+    //Чтобы не съедало кириллицу powershell'ом
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    while (gameState == 0) {
+
+        //Меню действий
+        printf("\n \n====== Меню =====\n"
+               "[0] Выход\n"
+               "[1] Посмотреть часы\n"
+               "[2] Промотать время(поработать)\n"
+               "[3] Посмотреть инвентарь\n"
+               "[4] Положить предмет в слот\n"
+               "[5] Выбросить предмет\n"
+               "[6] 6e6e6e6e\n"
+               "[7] лабуба\n"
+               "\n \n"
+        );
+
+        //Контр-дурак, первый этап
+        if (scanf("%d", &activeCall) != 1) {
+            while(getchar() != '\n');
+            printf("Придурок, введи один из вариантов действия\n");
+            continue;
+ 
+        }
+        
+        //Зачистка буфера если гений ввел 1abc
+        while  (getchar() != '\n');       
+
+        // printf("%d\n", activeCall); //затычка для проверки и дебага
+
+        
+        switch(activeCall) {
+            case 0:
+                gameState = 1;
+                break;
+            case 1:
+                printf("Текущее время: День %d, %02d:00\n", current_day, current_hour);   
+                break;
+            case 2:
+                //Важно! Использую activeCall（и буду использовать) так как это централизованная переменнаяя ввода данных
+                //в которой есть защита от дурня
+                printf("Было %d:00 %2d-го дня, сколько часов ты проведешь за работой: ", current_hour, current_day);
+                scanf("%d", &activeCall);
+                current_hour += activeCall;
+                if (current_hour >= 24) {
+                    current_day += current_hour / 24;
+                    current_hour = current_hour % 24;
+                }
+                activeCall = 0;
+                break;
+            case 3:
+                
+            default:
+                break;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+/*
+    if (scanf("%d", &activeCall) != 1) {
+        while(getchar() != '\n');
+            printf("Придурок, введи один из вариантов действия")
+         continue;
+    }
+
+    Защита от дурака, идея: 
+    сканим ввод сканф'ом, если пользователь ввел не число, то идет дальше в цикл getchar, который по одному символу его чистит
+    пока не дойдет до некст строки
+
+
+
+    //После присвоения нового Call'a пробуем чистить буфер чтобы мусор не копился в буфере
+    while  (getchar() != '\n');
+*/
